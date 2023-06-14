a875a59627d3b29e4d393dabcfcc63108b4be6cd
Wosen <wosen.yu72@gmail.com>
2023-06-14 16:50:35 -0400

    new

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore, collection, query } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCgSN_h3mK4i3vs9HcYY5LG0pmMvzL4DtI",
-    authDomain: "expensetracker-2eea5.firebaseapp.com",
-    projectId: "expensetracker-2eea5",
-    storageBucket: "expensetracker-2eea5.appspot.com",
-    messagingSenderId: "116229044858",
-    appId: "1:116229044858:web:0818bb6894a3981c3bd9c3",
-    measurementId: "G-YB7RRQV4EC"
+    apiKey: "AIzaSyBs7HFxYokQR9RkdutEF8BiziKS7Qb6kb8",
+    authDomain: "expense-chart-f7e88.firebaseapp.com",
+    projectId: "expense-chart-f7e88",
+    storageBucket: "expense-chart-f7e88.appspot.com",
+    messagingSenderId: "898817549479",
+    appId: "1:898817549479:web:7991c8b81f87a520bbe782",
+    measurementId: "G-ZCVK9K5Z0S"
 };
 
 // Initialize Firebase
